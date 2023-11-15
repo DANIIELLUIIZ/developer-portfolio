4097ed3b616ada505c626c23fba1c195b194ae8d
Daniel Freitas <danielluiz358@gmail.com>
2023-11-15 13:49:07 -0300

    update profile info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,46 @@
 function updateProfileInfo(profileData) {
-  const photo = document.querySelector('.profile-photo')
-  const name = document.querySelector('.profile-name')
-  const job = document.querySelector('.profile-job')
-  const location = document.querySelector('.profile-location')
-  const phone = document.querySelector('.profile-phone')
-  const email = document.querySelector('.profile-email')
-  // skills
-  const css = document.querySelector(".profile-css")
-
+  const photo = document.getElementById('profile.photo')
   photo.src = profileData.photo
   photo.alt = profileData.name
-  name.innerHTML = profileData.name
-  job.innerHTML = profileData.job
-  location.innerHTML = profileData.location
-  phone.innerHTML = profileData.phone
-  email.innerHTML = profileData.email
+
+  const name = document.getElementById('profile.name')
+  name.innerText = profileData.name
+
+  const job = document.getElementById('profile.job')
+  job.innerText = profileData.job
+
+  const location = document.getElementById('profile.location')
+  location.innerText = profileData.location
+
+  const phone = document.getElementById('profile.phone')
+  phone.innerText = profileData.phone
+  phone.href = `tel:${profileData.phone}`
+  const email = document.getElementById('profile.email')
+  email.innerText = profileData.email
+  email.href = `mailto:${profileData.email}`
+}
+function updateProfileHardSkills(profileData) {
+  const hardSkillsData = profileData.skills.hardSkills
+  const hardSkills = document.getElementById('profile.hardSkills')
+  for (let i = 0; i <= 0; i++) {
+    hardSkills.innerHTML += ` <li>
+<img src="${hardSkillsData[i].logo}"alt="${hardSkillsData[i].name}" />
+</li>
+`
+  }
+}
+
+function updateProfileSoftSkills(profileData) {
+  const softSkillsData = profileData.skills.softSkills
+  const softSkills = document.getElementById('profile.softSkills')
+  for (let i = 0; i < softSkillsData.length; i++) {
+    softSkills.innerHTML += `<li>${softSkillsData[i]}</li>`
+  }
 }
 
 ;(async () => {
   const profileData = await fetchProfileData()
   updateProfileInfo(profileData)
+  updateProfileSoftSkills(profileData)
+  updateProfileHardSkills(profileData)
 })()
