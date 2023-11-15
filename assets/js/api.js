b3a1e6756c9b54ae77b4a658d76d7d5c8f345aa7
Daniel Freitas <danielluiz358@gmail.com>
2023-11-15 12:30:40 -0300

    path profile info

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fetchProfileData() {
   const url =
-    'https://raw.githubusercontent.com/DANIIELLUIIZ/developer-portfolio/main/data/index.json'
+    'https://raw.githubusercontent.com/DANIIELLUIIZ/developer-portfolio/main/data/profile.json'
   const fetching = await fetch(url)
-  return fetching.json
+  return fetching.json()
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,21 @@
+function updateProfileInfo(profileData) {
+  const photo = document.querySelector('.profile-photo')
+  const name = document.querySelector('.profile-name')
+  const job = document.querySelector('.profile-job')
+  const location = document.querySelector('.profile-location')
+  const phone = document.querySelector('.profile-phone')
+  const email = document.querySelector('.profile-email')
+
+  photo.src = profileData.photo
+  photo.alt = profileData.name
+  name.innerHTML = profileData.name
+  job.innerHTML = profileData.job
+  location.innerHTML = profileData.location
+  phone.innerHTML = profileData.phone
+  email.innerHTML = profileData.email
+}
+
 ;(async () => {
   const profileData = await fetchProfileData()
-  console.log(profileData)
+  updateProfileInfo(profileData)
 })()
